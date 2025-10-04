332f8639ff13246c3eb50efabfd4990f7a29f8be
Maria Eduarda <dudaa.mendes1502@gmail.com>
2025-10-04 00:45:57 -0300

    feat: adicionar componentes layout e sidebar com responsividade

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import React, { ReactNode, useState } from 'react';
+import Sidebar from './Sidebar'; 
+import { Menu } from 'lucide-react';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className="flex min-h-screen">
+            <div className="lg:hidden fixed top-2 left-2 z-50">
+                <button
+                    className="p-2 rounded cursor-pointer hover:bg-[#4D44B5]/50 transition-colors duration-300"
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <Menu className="w-6 h-6 text-[#4D44B5]" />
+                </button>
+            </div>
+
+            <Sidebar isOpen={isOpen} setIsOpen={setIsOpen}/>
+
+            <main className={`flex-1 bg-[#F3F4FF] p-6 transition-opacity duration-300 !pt-14 lg:!pt-6 ${isOpen ? 'opacity-50' : ''}`}>
+                {children}
+            </main>
+        </div>
+    );
+};
+
+export default Layout;
---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { House, Users, UserPlus, User, HandCoins, X } from 'lucide-react';
+
+interface SidebarProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Sidebar = ({ isOpen, setIsOpen }: SidebarProps) => {
+  const pathname = usePathname();
+
+  const links = [
+    { label: 'Dashboard', href: '/', icon: House },
+    { label: 'Students', href: '/students', icon: Users },
+    { label: 'Add Student', href: '/add-student', icon: UserPlus },
+    { label: 'Teachers', href: '/teachers', icon: Users },
+    { label: 'Finance', href: '/finance', icon: HandCoins },
+    { label: 'User', href: '/user', icon: User },
+  ];
+
+  return (
+    <div className={`fixed lg:static top-0 left-0 z-50 w-3/4 lg:w-72 h-screen bg-[#4D44B5] shadow-md p-6 !pr-0 flex flex-col transform transition-transform duration-300
+                     ${isOpen ? 'translate-x-0' : '-translate-x-full'}
+                     lg:translate-x-0`}>
+        <div className='lg:hidden flex justify-end pr-4'>
+          <button
+              className="p-2 cursor-pointer"
+              onClick={() => setIsOpen(false)}
+          >
+              <X className="w-6 h-6 text-white" />
+          </button>
+        </div>
+        <div className='flex items-center justify-center gap-3 p-6 !pr-12'>
+          <span className='text-3xl font-bold px-2 py-0.5 rounded-2xl bg-[#FB7D5B]'>A</span>
+          <h1 className="text-3xl font-bold">Akademi</h1>
+        </div>
+
+        <nav className="flex flex-col gap-4 p-4 !pr-0">
+            {links.map((link) => {
+            const Icon = link.icon || (() => null);  
+            const isActive = pathname === link.href;
+            return (
+                <Link
+                key={link.href}
+                href={link.href}
+                onClick={() => setIsOpen(false)} 
+                className={`px-6 py-4 rounded-l-full flex items-center gap-4 ${
+                    isActive ? 'text-[#4D44B5] bg-[#F3F4FF] font-medium' : 
+                               'text-white hover:bg-[#F3F4FF]/85 hover:text-[#4D44B5] transition-colors duration-200'
+                }`}
+                >
+                <Icon size={24} />
+                {link.label}
+                </Link>
+            );
+            })}
+        </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
