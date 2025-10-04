06286fddab99e32b751989a4b2df0fde31ffaa3a
Maria Eduarda <dudaa.mendes1502@gmail.com>
2025-10-04 13:36:04 -0300

    feat: implementar componente de bagde

---
+++ src/components/layout/Badge.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React from 'react';
+
+interface BadgeProps {
+    text?: string;
+    color?: string; 
+    isCircle?: boolean;
+    textColor?: string; 
+    onClick?: () => void;
+    icon?: React.ReactNode;
+}
+
+const Badge = ({ text, icon, color = '#4D44B5', textColor = 'white', isCircle, onClick }: BadgeProps) => {
+    const baseClasses = `inline-flex items-center justify-center font-medium text-xs`;
+    const shapeClasses = isCircle ? 'rounded-full p-2 cursor-pointer select-none' : 'rounded-full px-2 py-0.5 gap-1';
+    const combinedClasses = `${baseClasses} ${shapeClasses}`;
+    
+    return (
+        <div
+            onClick={onClick}
+            className={combinedClasses}
+            style={{ backgroundColor: color, color: textColor }}
+        >
+            {icon && <span>{icon}</span>}
+            {text && <span>{text}</span>}
+        </div>
+    );
+};
+
+export default Badge;
