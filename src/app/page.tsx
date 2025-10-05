35a0f1a9ee0d3e15e013ea6a18481ba1a9664a13
Maria Eduarda <dudaa.mendes1502@gmail.com>
2025-10-05 16:09:42 -0300

    feat: adicionar pages de Teachers, Finance e User

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Header from '@/components/layout/Header';
 import Layout from '@/components/layout/Layout';
 
 
 export default function Home() {
   return (
     <Layout>
-      <h1 className="text-3xl font-bold text-[#4D44B5] mb-6">-</h1>
+      <Header title="Dashboard" />
     </Layout>
   );
 }
---
+++ src/app/teachers/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/layout/Header";
+import Layout from "@/components/layout/Layout";
+
+export default function TeachersPage() {
+  return (
+    <Layout>
+      <Header title="Teachers" />
+    </Layout>
+  );
+}
